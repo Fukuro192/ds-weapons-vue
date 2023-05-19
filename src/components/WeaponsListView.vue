8aecbbfd472f01e167a4f9b635cb49426525020e
Fukuro192 <salah8elmes.2000@gmail.com>
2023-05-19 10:29:10 +0200

    add query search

--- src/components/WeaponsListView.vue
+++ src/components/WeaponsListView.vue
@@ -1,37 +1,46 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-defineProps({
-    weapons: Array
-})
+import { ref } from 'vue';
 function clickOnRouter(router_id) {
     document.getElementById(router_id).click();
+}
+
+let weapons = ref([]);
+fetch("http://localhost:3001/api/weapons")
+  .then(response => response.json())
+  .then(data => weapons.value = data);
+console.log("in WeaponsListView: ", weapons);
+let query = ref('');
+function findWeapons() {
+  fetch("http://localhost:3001/api/weapons?name="+query.value)
+  .then(response => response.json())
+  .then(data => weapons.value = data);
 }
 </script>
 
 <template>
-    <main>
-        <div style="margin: 0 auto;">
-            <ul class="overflow-x-hidden overflow-y-auto h-96">
-                <li
-                    v-for="weapon in weapons" :key="weapon.id"
-                    @click="clickOnRouter(`weapon_detail_${weapon.id}`)"
-                >
-                    <span>{{ weapon.id }}</span>
-                    <span>{{ weapon.name }}</span>
-                    <span>Atk: {{ weapon.attack.physical }}</span>
-                    <span>Def: {{ weapon.defence.physical }}</span>
-                    <RouterLink :id="`weapon_detail_${weapon.id}`" :to="`/details/${weapon.id}`"></RouterLink>
-                </li>
-            </ul>
-        </div>
-    </main>
+    <div class="grid grid-cols-1 gap-0 px-20" style="margin: 0 auto;" v-if="weapons">
+        <input placeholder="Search, Ex: Moonlight Greatsword" class="text-black mx-max" type="text" v-model="query" @keyup="findWeapons()">
+        <ul class="overflow-x-hidden overflow-y-auto h-96">
+            <li
+                v-for="weapon in weapons" :key="weapon.id"
+                @click="clickOnRouter(`weapon_detail_${weapon.id}`)"
+            >
+                <span>{{ weapon.id }}</span>
+                <span>{{ weapon.name }}</span>
+                <span>Atk: {{ weapon.attack.physical }}</span>
+                <span>Def: {{ weapon.defence.physical }}</span>
+                <RouterLink :id="`weapon_detail_${weapon.id}`" :to="`/details/${weapon.id}`"></RouterLink>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
 ul {
     position: relative;
     width: 450px;
-    margin: 100px auto 0;
+    margin: 0px auto 0;
     padding: 10px;
     box-sizing: border-box;
     background: rgba(0, 0, 0, .1);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,9 @@
 <script setup>
 import WeaponsListView from '../components/WeaponsListView.vue';
-import { ref } from 'vue';
-let weapons = ref([]);
-fetch("http://localhost:3001/api/weapons")
-  .then(response => response.json())
-  .then(data => weapons.value = data);
-console.log("in HomeView: ", weapons);
 </script>
 
 <template>
-  <WeaponsListView :weapons="weapons" />
+  <main>
+    <WeaponsListView :weapons="weapons" />
+  </main>
 </template>
